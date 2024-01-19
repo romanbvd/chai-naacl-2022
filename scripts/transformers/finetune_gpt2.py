6b76680f68853c5a50892693f12e64add49674fa
romanbvd <romanbvd@gmail.com>
2024-01-19 22:04:02 +0200

    fix: finetuning config changed

--- scripts/transformers/finetune_gpt2.py
+++ scripts/transformers/finetune_gpt2.py
@@ -89,10 +89,10 @@ if __name__ == "__main__":
         per_device_eval_batch_size=args.batch_size,
         fp16=args.fp16,
         no_cuda=False,
-        num_train_epochs=15,
+        num_train_epochs=7,
         save_steps=1000,
         warmup_steps=100,
-        save_total_limit=3,
+        save_total_limit=2,
         dataloader_drop_last=True,
     )
     trainer = Trainer(
